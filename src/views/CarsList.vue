e69c64638a3da360ebce6bd39ae3c26a8b90a27a
Runatorium <stefano.alesci1@gmail.com>
2023-06-05 16:58:13 +0200

    task 1 forse completata

---
+++ src/views/CarsList.vue
@@ -0,0 +1,39 @@
+
+<script>
+import SingleCar from '../components/SingleCar.vue';
+import SingleCar from '../components/SingleCar.vue';
+import SingleCar from '../components/SingleCar.vue';
+import axios from 'axios';
+
+  export default{
+    name: "CarsList",
+    data() {
+        return {
+            carsList: [],
+            UrlBase: "http://localhost:8000"
+        };
+    },
+    methods: {
+        async getCars() {
+            await axios.get(`${UrlBase}/api/cars`).then(response => {
+                this.carsList = response.data.results.data;
+            });
+        }
+    },
+    mounted() {
+        this.getCars();
+    },
+    components: { SingleCar }
+}
+</script>
+
+
+<template>
+  <div v-for="car in carsList">
+  <SingleCar :SingleCar="car"/>
+  </div>
+</template>
+
+<style>
+
+</style>
